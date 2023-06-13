f597dfa053f503fda21f7644de02d1c2b6c2c62a
LIRA <aurielharlih9213@gmail.com>
2023-06-14 06:56:25 +0700

    setup api endpoint

---
+++ src/utils/api-endpoints.js
@@ -0,0 +1,42 @@
+export const API_ENDPOINTS = {
+  LOGIN: "/auth/login",
+  // PRODUCT
+  GET_ALL_PRODUCTS: "/product/",
+  CREATE_PRODUCT: "/product/",
+  UPLOAD_SKETCH_PRODUCT: "/product/image/denah/upload/",
+  UPLOAD_IMAGES_PRODUCT: "/product/image/slide/upload/",
+  DELETE_IMAGES_PRODUCT: "/product/images/array?images=",
+  DELETE_PRODUCT: "/product/",
+  // USER
+  GET_ALL_USERS: "/user/",
+  GET_USER: "/user/",
+  CREATE_USER: "/user/",
+  UPDATE_USER: "/user/",
+  DELETE_USER: "/user/",
+  //GALLERY
+  GET_ALL_GALLERY: "/gallery",
+  POST_GALLERY_TITLE: "/gallery",
+  POST_GALLERY_IMAGE: "/gallery/image/upload/",
+  PATCH_GALLERY_TITLE: "/gallery/",
+  DELETE_GALLERY: "/gallery/",
+  DELETE_GALLERY_IMAGE: "/gallery/image/",
+  DELETE_GALLERY_IMAGE_ARRAY: "/gallery/images/array",
+  //PARTNER
+  GET_ALL_PARTNER: "/partner",
+  POST_PARTNER: "/partner",
+  POST_PARTNER_IMAGE: "/partner/image/upload/",
+  PATCH_PARTNER: "/partner/",
+  DELETE_PARTNER: "/partner/",
+  // BLOG
+  GET_BLOG: "/blog",
+  POST_BLOG: "/blog",
+  PATCH_BLOG: "/blog",
+  DELETE_BLOG: "/blog",
+  POST_IMAGE_BLOG: "/blog/image/upload/",
+  //PROMO
+  GET_PROMO: "/promo",
+  POST_PROMO: "/promo",
+  PATCH_PROMO: "/promo",
+  DELETE_PROMO: "/promo",
+  POST_IMAGE_PROMO: "/promo/image/upload/",
+};
