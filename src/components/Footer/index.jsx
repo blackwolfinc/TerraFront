19dd401f82b34ca35203e3a50c913584b5dea2ad
LIRA <aurielharlih9213@gmail.com>
2023-06-14 12:48:54 +0700

    add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import TerraLogoWhite from "assets/img/terra-logo-white.png";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import { HiOutlineMail, HiPhone } from "react-icons/hi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-full bg-dark-primary">
+      <div className="mx-auto flex max-w-7xl">
+        <div className="w-1/2 bg-dark-primary px-10 py-20 font-baijamjuree text-white">
+          <img src={TerraLogoWhite} alt="Terra Logo" className="mb-6 w-60" />
+          <div className="mb-9">
+            <span>
+              At TERRAKOTA RESIDANCE
+              <br />
+              We Build Your Story.
+            </span>
+          </div>
+          <div className="mb-6 flex gap-4">
+            <FaFacebook size={36} />
+            <FaInstagram size={36} />
+            <FaTiktok size={36} />
+          </div>
+          <p className="text-xs leading-relaxed">
+            For over 30 years, TERRAKOTA RESIDANCE develops and manages
+            affordable to high-end homes, integrated communities,and more for
+            the purpose of elevating lifestyles and improving the lives of
+            everyone we touch.
+          </p>
+        </div>
+        <div className="flex items-center bg-primary">
+          <div className="flex flex-1 gap-8 px-10">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.372306013702!2d111.89092857671346!3d-8.063453671244664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78e2dd9dbdf3e3%3A0x540402dd85e005f3!2sJl.%20Mayjend%20Sungkono%20No.84%2C%20Kutoanyar%2C%20Kec.%20Tulungagung%2C%20Kabupaten%20Tulungagung%2C%20Jawa%20Timur%2066219!5e0!3m2!1sid!2sid!4v1686721217281!5m2!1sid!2sid"
+              width={360}
+              height={240}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+            <div className="font-baijamjuree text-white">
+              <h1 className="mb-10 text-2xl font-bold">Contact Us</h1>
+              <div className="mb-10">
+                <div className="flex items-center gap-2">
+                  <HiOutlineMail size={24} />
+                  <span>terrabumipesada@gmail.com</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <HiPhone size={24} />
+                  <span>0811-320-45678</span>
+                </div>
+              </div>
+              <p>
+                Jl. Mayjend Sungkono No.84, Kutoanyar, Kec. Tulungagung,
+                Kabupaten Tulungagung, Jawa Timur 66219
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
