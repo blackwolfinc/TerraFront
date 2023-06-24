3ddba33f08c84fe6e7c2620c67cb4d9c69fd0ed8
E Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-06-24 11:25:56 +0700

    connect API n responsive

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -45,9 +45,7 @@ const ProductsList = () => {
             />
             {/**/}
             <div className="text-sm sm:text-base">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type
+              {productData?.description}
             </div>
           </div>
         </div>
